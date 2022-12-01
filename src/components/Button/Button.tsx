bd6779b8d2e38e26624d91a3624bb6e942bd6f68
Juan Vivanco <juansebastianvc@gmail.com>
2022-12-01 00:55:22 -0500

    delete compontnt

--- src/components/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { Button as NDPButton } from '@zenprojects/ndp-design-system';
-
-export const Button = ({ children }: { children: React.ReactNode }): JSX.Element => {
-  return <NDPButton>{children}</NDPButton>;
-};
